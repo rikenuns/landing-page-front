4794602db594764875ea8bce53ccace9454f8b58
rikenuns <deividhenrique1020@gmail.com>
2026-05-25 23:02:11 -0300

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+git commit "kqwdl"pppppimport { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
